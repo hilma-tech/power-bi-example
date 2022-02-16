71a2640521cc42d781275d17f9a24d5437ed4e6e
michaelseg11 <michaelseg11@gmail.com>
2022-02-16 17:48:57 +0200

    change client secret

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
     clientId: "8d90deae-4426-4cee-88c5-525eaba2440d",
-    clientSecret: "hp_7Q~JPCMUaShdSu-nGRofVgyOUDdC7PfTnF",
+    clientSecret: "s0P7Q~cy4sCMX~A6OautxPI_aMAdkLsggVRUv",
     tenantId: "e8862814-7c70-42ce-ae78-33cb801e5cbf",
     workspaceId: "766d77c5-d2d5-483a-abe2-62464aaf4e69",
     reportId: "aa728082-45cb-49a3-8242-7f2abdd898e3"
